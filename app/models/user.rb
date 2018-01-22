879ce7da1ac1557cdd33270ed4937cd67ad0b3fd
hujwala <upatel@qwinixtech.com>
2018-01-22 11:36:15 +0530

    css

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,2 @@
 class User < ActiveRecord::Base
-
-  has_secure_password :validations => false
-  validates :name, presence: true
-
-  validates :email, presence: true
-
-  validates :password, :presence => true
-
-
 end
