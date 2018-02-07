d5c8de9a1600ff9445d2e373a53a023558a3459e
hujwala <upatel@qwinixtech.com>
2018-02-07 18:21:33 +0530

    css

--- app/controllers/speciality_controller.rb
+++ app/controllers/speciality_controller.rb
@@ -1,4 +1,6 @@
 class SpecialityController < ApplicationController
+ before_action :hospital
+
   def index
     @speciality = Speciality.all
   end
@@ -19,7 +21,18 @@ end
   def new
   end
 
+  #   def destroy
+  #   @speciality = Speciality.find_by(:id => params[:id])
+  #   @speciality.destroy
+  #   flash[:notice] = "hospital deleted successfully!"
+  #   redirect_to speciality_index_path
+  # end
+
   private
+
+  def hospital
+  @speciality = Speciality.find_by(:id => params[:id])
+end
 
     def speciality_params
       params.require(:speciality).permit( :speciality_name, :doctor_name, :doctor_venue, :phone, :hospital_name)
