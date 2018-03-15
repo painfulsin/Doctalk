e2511c81261b3e45817afcd29e131e534f350c6d
shashikirans <shashikiran3110@gmail.com>
2018-03-15 16:51:28 +0530

    added

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :rbenv_ruby, "2.3.1p112"
 set :application, "Doctalk"
 set :repo_url, "git@github.com:painfulsin/Doctalk.git"
 # set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle}
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/security.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/security.yml','config/secrets.yml')
 # set :default_env, { path: "/home/deploy/.rbenv/shims:/home/deploy/.rbenv/bin:$PATH" }
 set :bundle_flags, '--deployment --quiet'
 set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
