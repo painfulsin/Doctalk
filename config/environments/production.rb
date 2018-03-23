274a3a8a87f007432eec95094fd4596621ad3d0b
hujwala <upatel@qwinixtech.com>
2018-03-23 12:36:08 +0530

    spacing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,8 +56,8 @@ Rails.application.configure do
         :address    => "smtp.sendgrid.net",
         :port       => 587,
         :domain     => "heroku.com",
-        :user_name  => ENV["SENDGRID_USERNAME"],
-        :password  =>  ENV["SENDGRID_PASSWORD"],
+        :user_name  => "shashikiran31",
+        :password  =>  "Password@1",
         :authentication   => :plain,
         :enable_starttls_auto => true
    }
