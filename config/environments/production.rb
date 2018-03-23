5d42d60a84645c3e15923f39dbf42b0380aeeae5
hujwala <upatel@qwinixtech.com>
2018-03-23 12:23:54 +0530

    spacing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
         ActionMailer::Base.smtp_settings = {
         :address    => "smtp.sendgrid.net",
         :port       => 587,
-        :domain     => "sendgrid.com",
+        :domain     => "heroku.com",
         :user_name  => ENV["SENDGRID_USERNAME"],
         :password  => ENV["SENDGRID_PASSWORD"],
         :authentication   => :login,
