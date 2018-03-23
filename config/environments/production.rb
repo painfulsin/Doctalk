e709b7becd730ca5761ca80c18fb81fd189eea38
hujwala <upatel@qwinixtech.com>
2018-03-23 12:31:06 +0530

    spacing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,16 +50,15 @@ Rails.application.configure do
 #    }
 # end
 
-
+ActionMailer::Base.delivery_method = :smtp
 Rails.application.configure do
-      # config.action_mailer.default_url_options ={:host => "localhost:3000"}
         ActionMailer::Base.smtp_settings = {
         :address    => "smtp.sendgrid.net",
         :port       => 587,
         :domain     => "heroku.com",
         :user_name  => ENV["SENDGRID_USERNAME"],
-        :password  => ENV["SENDGRID_PASSWORD"],
-        :authentication   => :login,
+        :password  =>  ENV["SENDGRID_PASSWORD"],
+        :authentication   => :plain,
         :enable_starttls_auto => true
    }
    end
