1cdd0af2ceeb4725f55132c82f206511f0e58e50
nikhil405 <npraksh@qwinix.io>
2018-06-13 16:16:12 +0530

    gem adding

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,18 +1,18 @@
-# set path to application
-app_dir = File.expand_path("/u01/apps/qwinix/doctalk/current",
+# # set path to application
+# app_dir = File.expand_path("/u01/apps/qwinix/doctalk/current",
 
-__FILE__)
-#shared_dir = "#{app_dir}/shared"
-working_directory app_dir
+# __FILE__)
+# #shared_dir = "#{app_dir}/shared"
+# working_directory app_dir
 
-# Set unicorn options
-worker_processes 2
-preload_app true
-timeout 30
-# Set up socket location
-listen "/u01/apps/qwinix/doctalk/shared/sockets/unicorn.sock", :backlog => 64
-# Logging
-stderr_path "/u01/apps/qwinix/doctalk/current/log/unicorn.stderr.log"
-stdout_path "/u01/apps/qwinix/doctalk/current/log/unicorn.stdout.log"
-# Set master PID location
-pid "/u01/apps/qwinix/doctalk/shared/pids/unicorn.pid"
+# # Set unicorn options
+# worker_processes 2
+# preload_app true
+# timeout 30
+# # Set up socket location
+# listen "/u01/apps/qwinix/doctalk/shared/sockets/unicorn.sock", :backlog => 64
+# # Logging
+# stderr_path "/u01/apps/qwinix/doctalk/current/log/unicorn.stderr.log"
+# stdout_path "/u01/apps/qwinix/doctalk/current/log/unicorn.stdout.log"
+# # Set master PID location
+# pid "/u01/apps/qwinix/doctalk/shared/pids/unicorn.pid"
